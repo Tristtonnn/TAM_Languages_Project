0630c0e951a897abf12a64ef9c05c2dd85de7a57
tamitchell <tamitchell@CS100-VBox-TAM.myguest.virtualbox.org>
2023-11-30 08:58:49 -0500

    first commit

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# create directories for the assignment
+mkdir images
+mkdir ping_output
+mkdir processed_images
+cd processed_images
+mkdir large
+mkdir small
+
+# print success message echo "Directories created successfully"
